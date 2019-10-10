3299b0a109692be1644ea8767f87c5ce80e423bc
Peter Stratton <peterstratton121@gmail.com>
2019-10-09 17:05:46 -0700

    Click and draw Rectangles

--- Environment.py
+++ Environment.py
@@ -4,15 +4,27 @@ import numpy as np
 import Rectangle
 from Rectangle import *
 
+# Cosntants
 SCREEN_X = 1000
 SCREEN_Y = 1000
 RECT_COLOR = 'blue'
 
-turtle.screensize(SCREEN_X, SCREEN_Y)
-turtle.ht()
+def main():
+    turtle.screensize(SCREEN_X, SCREEN_Y)
+    turtle.ht()
 
-r = Rectangle(0, 0, 50, 50, RECT_COLOR)
-r.test()
+    turtle.listen() # listen for mouse events
+    turtle.onscreenclick(clickDraw)
+
+    turtle.mainloop()
 
 
+
+def clickDraw(x, y):
+    print("New Rectangle at: " + str(x) + " " + str(y))
+    r = Rectangle(y, x, 50, 50, RECT_COLOR)
+    r.draw_rect()
+
+
+main()
 turtle.done()
--- Rectangle.py
+++ Rectangle.py
@@ -5,18 +5,24 @@ import numpy as np
 class Rectangle:
 
     def __init__(self, x, y, width, height, color):
-            self.topLeftX = x - (width / 2)
-            self.bottomLeftX = self.topLeftX
-            self.topRightX = x + (width / 2)
-            self.bottomRightX = self.topLeftX
+        self.color = color
+        self.topLeftX = x - (width / 2)
+        self.bottomLeftX = self.topLeftX
+        self.topRightX = x + (width / 2)
+        self.bottomRightX = self.topRightX
 
-            self.topLeftY = y + (height / 2)
-            self.bottomLeftY = y - (height / 2)
-            self.topRightY = self.topLeftY
-            self.bottomRightY = self.bottomLeftY
+        self.topLeftY = y + (height / 2)
+        self.bottomLeftY = y - (height / 2)
+        self.topRightY = self.topLeftY
+        self.bottomRightY = self.bottomLeftY
 
     def draw_rect(self):
         turtle.speed(0)
+        turtle.ht()
+        turtle.color(self.color)
+        turtle.fillcolor(self.color)
+
+        turtle.begin_fill()
 
         turtle.up()
         turtle.goto(self.topLeftX, self.topLeftY)
@@ -24,3 +30,7 @@ class Rectangle:
         turtle.goto(self.topRightX, self.topRightY)
         turtle.goto(self.bottomRightX, self.bottomRightY)
         turtle.goto(self.bottomLeftX, self.bottomLeftY)
+        turtle.goto(self.topLeftX, self.topLeftY)
+
+        turtle.end_fill()
+        turtle.done()
